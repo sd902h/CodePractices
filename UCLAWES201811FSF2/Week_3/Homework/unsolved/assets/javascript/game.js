9e970c4fe2f079c5c02bc256a6c75a14a8c7d357
Sonia D <davson1987@gmail.com>
2023-07-11 16:59:02 -0700

    Day 2 Week 4 exercises complete

--- UCLAWES201811FSF2/Week_3/Homework/unsolved/assets/javascript/game.js
+++ UCLAWES201811FSF2/Week_3/Homework/unsolved/assets/javascript/game.js
@@ -2,7 +2,7 @@
 //use math.random to generate random word
 //use query selector to display length of selected word
 
-var wordsArray = ["happy", "bunny", "cheese"];
+var wordsArray = ["happy", "rice", "roy"];
 
 const alpha = Array.from(Array(26)).map((e, i) => i + 65);
 const alphabet = alpha.map(x => String.fromCharCode(x));
@@ -11,24 +11,29 @@ console.log(alphabet);
 function onlyLettersAndNumbers(str) {
   return Boolean(str.match(/^[A-Za-z0-9]*$/));
 }
-console.log(onlyLettersAndNumbers(wordsArray[0]));
+console.log(onlyLettersAndNumbers(wordsArray[1]));
 
-function replaceAll(string, search, replace) {
-  return string.split(search).join(replace);
-}
-
-console.log(replaceAll(wordsArray[0], alphabet, "_ "));
+//Attempted to apply the above to an array
+// for (i = 0; (i = wordsArray.length); i++) {
+//   console.log(Boolean(wordsArray[i].match(/^[A-Za-z0-9]*$/)));
+// }
 
 const searchRegExp = /^[A-Za-z0-9]*$/gi;
-const replaceWith = "_";
+const replaceWith = "_ ";
 
-const result = wordsArray[0].replace(searchRegExp, replaceWith);
+const result = wordsArray[0].replace(
+  searchRegExp,
+  replaceWith.repeat(wordsArray[0].length)
+);
 console.log(result);
+
+var word = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+console.log(word);
+var result1 = word.replace(searchRegExp, replaceWith.repeat(word.length));
+console.log(result1);
 
 //Attempt 1 it returns "_" when I want the outcome to be " - - - - -"
 var getRandomWord = function() {
-  var word = wordsArray[Math.floor(Math.random() * wordsArray.length)];
-  console.log(word);
   for (i = 0; (i = word.length); i++) {
     var change = word.charAt(i).replaceAll(searchRegExp, replaceWith);
     return change;
